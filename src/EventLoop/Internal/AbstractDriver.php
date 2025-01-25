09bf1bf7f7f574453efe43044b06fafe12216eb3
Aaron Piotrowski <aaron@trowski.com>
2025-01-25 13:27:39 -0600

    Explicitly forbid serialization and deserialization of driver

--- src/EventLoop/Internal/AbstractDriver.php
+++ src/EventLoop/Internal/AbstractDriver.php
@@ -639,4 +639,14 @@ abstract class AbstractDriver implements Driver
             }
         };
     }
+
+    final public function __serialize(): never
+    {
+        throw new \Error(__CLASS__ . ' does not support serialization');
+    }
+
+    final public function __unserialize(array $data): never
+    {
+        throw new \Error(__CLASS__ . ' does not support deserialization');
+    }
 }
